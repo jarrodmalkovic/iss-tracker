ee92ff494a6885088d58a9745699d70255c3bf35
jarrod-ma <jarrodmalkovic@outlook.com>
2020-11-21 07:39:03 +0800

    Added more information

--- src/components/InformationBox.js
+++ src/components/InformationBox.js
@@ -1,14 +1,13 @@
-import { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
     box: {
         position: "absolute",
-        top: "30px",
-        left: "30px",
+        top: "20px",
+        left: "20px",
         padding: "20px",
-        maxWidth: "400px",
+        maxWidth: "405px",
         backgroundColor: "rgba(0, 0, 0, 0.5)",
         zIndex: 9999,
         borderRadius: "5px",
@@ -19,33 +18,14 @@ const useStyles = makeStyles({
 
 function LocationInfoBox({ issPosition }) {
     const classes = useStyles();
-    const [currentIssSpeed, setCurrentIssSpeed] = useState('Calculating...');
-    const [prevIssPosition, setPrevIssPosition] = useState({});
-
-    useEffect(() => {
-        calculateIssSpeed(issPosition.latitude, issPosition.longitude);
-    }, [issPosition.latitude, issPosition.longitude])
-
-    // Using the Haversine formula, we are able to calculate the distance between two points and thus determine the speed
-    // https://en.wikipedia.org/wiki/Haversine_formula
-    function calculateIssSpeed(latitude, longitude) {
-        if (!Object.keys(prevIssPosition).length) return setPrevIssPosition({ latitude, longitude });
-        const earthRadius = 6371e3;
-        const phi1 = prevIssPosition.latitude * Math.PI / 180;
-        const phi2 = latitude * Math.PI / 180;
-        const dPhi = (latitude - prevIssPosition.latitude) * Math.PI / 180;
-        const dLambda = (longitude - prevIssPosition.longitude) * Math.PI / 180;
-        const a = Math.sin(dPhi / 2) * Math.sin(dPhi / 2) + Math.cos(phi1) * Math.cos(phi2) * Math.sin(dLambda / 2) * Math.sin(dLambda / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const distanceTravelled = earthRadius * c;
-        setCurrentIssSpeed(`${(distanceTravelled / 5).toFixed(4)} m/s`)
-        setPrevIssPosition({ latitude, longitude });
-    }
 
     return (
         <div className={classes.box}>
             <Typography variant="h6" className={classes.title}>
-                International Space Station Tracker
+                Realtime International Space Station Tracker
+            </Typography>
+            <Typography variant="subtitle2">
+                <a href="https://www.nasa.gov/multimedia/nasatv/iss_ustream.html" target="_blank" rel="noreferrer">Click here to view live video feed from the ISS here</a>
             </Typography>
             <Typography variant="subtitle1">
                 Current Latitude: {issPosition.latitude.toFixed(4)}
@@ -54,10 +34,16 @@ function LocationInfoBox({ issPosition }) {
                 Current Longitude: {issPosition.longitude.toFixed(4)}
             </Typography>
             <Typography variant="subtitle1">
-                Current Speed: {currentIssSpeed}
+                Current Velocity: {issPosition.velocity.toFixed(4)} {issPosition.units === 'kilometers' ? 'km/h' : 'm/h'}
+            </Typography>
+            <Typography variant="subtitle1">
+                Current Altitude: {issPosition.altitude.toFixed(4)} {issPosition.units === 'kilometers' ? 'km' : 'miles'}
+            </Typography>
+            <Typography variant="subtitle1">
+                Current Visibility: {issPosition.visibility}
             </Typography>
             <Typography variant="subtitle2">
-                This information is updated every five seconds
+                This information is updated every 3000 milliseconds using data from wheretheiss.at
             </Typography>
         </div >
     )
